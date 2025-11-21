714ea495ff6fcf84abe151e83d7c2b93831292c4
IanMai0 <ian2000xz@gmail.com>
2025-11-21 15:58:54 +0800

    251121 Update

---
+++ 自動化網爬架構/main_v2.py
@@ -0,0 +1,16 @@
+# import 必要套件
+
+# === import 爬蟲模組 ===
+# import 國貿局網爬區塊
+from lab_crawler.優化後空間.國貿局.lab_250930v2 import main
+
+# import GCIS 網爬區塊
+from lab_crawler.優化後空間.GCIS import *
+# import 工廠 網爬區塊
+from lab_crawler.優化後空間.工廠 import *
+# import 稅籍 網爬區塊
+from lab_crawler.優化後空間.稅籍 import *
+
+# import SQL 模組
+
+
